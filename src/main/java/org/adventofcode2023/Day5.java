edca60d0f8d9bf857a57d338aed685ee0f9aa6d2
Susan Fung <fung_susan@hotmail.com>
2023-12-11 21:19:56 -0700

    ! F Created lowestLocationNumber method

--- src/main/java/org/adventofcode2023/Day5.java
+++ src/main/java/org/adventofcode2023/Day5.java
@@ -1,10 +1,46 @@
 package org.adventofcode2023;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Day5 {
+    public static Integer lowestLocationNumber(List<Integer> seedList,
+                                               List<String> seedToSoilList,
+                                               List<String> soilToFertilizerList,
+                                               List<String> fertilizerToWaterList,
+                                               List<String> waterToLightList,
+                                               List<String> lightToTemperatureList,
+                                               List<String> temperatureToHumidityList,
+                                               List<String> humidityToLocationList) {
+        Map<Integer, Integer> seedToSoilMap = createMap(seedToSoilList);
+        Map<Integer, Integer> soilToFertilizerMap = createMap(soilToFertilizerList);
+        Map<Integer, Integer> fertilizerToWaterMap = createMap(fertilizerToWaterList);
+        Map<Integer, Integer> waterToLightMap = createMap(waterToLightList);
+        Map<Integer, Integer> lightToTemperatureMap = createMap(lightToTemperatureList);
+        Map<Integer, Integer> temperatureToHumidityMap = createMap(temperatureToHumidityList);
+        Map<Integer, Integer> humidityToLocationMap = createMap(humidityToLocationList);
+
+        Set<Integer> locations = new HashSet<>();
+
+        for (int seed : seedList) {
+            int soil = (seedToSoilMap.get(seed) == null) ? seed : seedToSoilMap.get(seed);
+            int fertilizer = (soilToFertilizerMap.get(soil) == null) ? soil : soilToFertilizerMap.get(soil);
+            int water = (fertilizerToWaterMap.get(fertilizer) == null) ? fertilizer : fertilizerToWaterMap.get(fertilizer);
+            int light = (waterToLightMap.get(water) == null) ? water : waterToLightMap.get(water);
+            int temperature = (lightToTemperatureMap.get(light) == null) ? light : lightToTemperatureMap.get(light);
+            int humidity = (temperatureToHumidityMap.get(temperature) == null) ? temperature : temperatureToHumidityMap.get(temperature);
+            int location = (humidityToLocationMap.get(humidity) == null) ? humidity : humidityToLocationMap.get(humidity);
+
+            locations.add(location);
+        }
+
+        return Collections.min(locations);
+    }
+
     public static Map<Integer, Integer> createMap(List<String> stringList) {
         Map<Integer, Integer> map = new HashMap<>();
 
--- src/test/java/org/adventofcode2023/Day5Test.java
+++ src/test/java/org/adventofcode2023/Day5Test.java
@@ -8,6 +8,27 @@ import java.util.List;
 
 class Day5Test {
     @Test
+    void lowestLocationNumber() {
+        List<Integer> seedList = Arrays.asList(79, 14, 55, 13);
+        List<String> seedToSoilList = Arrays.asList("50 98 2", "52 50 48");
+        List<String> soilToFertilizerList = Arrays.asList("0 15 37", "37 52 2", "39 0 15");
+        List<String> fertilizerToWaterList = Arrays.asList("49 53 8", "0 11 42", "42 0 7", "57 7 4");
+        List<String> waterToLightList = Arrays.asList("88 18 7", "18 25 70");
+        List<String> lightToTemperatureList = Arrays.asList("45 77 23", "81 45 19", "68 64 13");
+        List<String> temperatureToHumidityList = Arrays.asList("0 69 1", "1 0 69");
+        List<String> humidityToLocationList = Arrays.asList("60 56 37", "56 93 4");
+
+        Approvals.verify(Day5.lowestLocationNumber(seedList,
+                                                   seedToSoilList,
+                                                   soilToFertilizerList,
+                                                   fertilizerToWaterList,
+                                                   waterToLightList,
+                                                   lightToTemperatureList,
+                                                   temperatureToHumidityList,
+                                                   humidityToLocationList));
+    }
+
+    @Test
     void createMap() {
         List<String> seedToSoilMap = Arrays.asList("50 98 2", "52 50 48");
         Approvals.verify(Day5.createMap(seedToSoilMap));
