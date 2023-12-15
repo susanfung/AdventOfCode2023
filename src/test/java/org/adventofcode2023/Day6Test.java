21452cc3a38f85ec524d08256352e282ea4465cf
Susan Fung <fung_susan@hotmail.com>
2023-12-15 12:39:18 -0700

    . F Answer Day 6 Part 1

--- src/test/java/org/adventofcode2023/Day6Test.java
+++ src/test/java/org/adventofcode2023/Day6Test.java
@@ -8,6 +8,13 @@ import java.util.List;
 
 class Day6Test {
     @Test
+    void answerDay6Part1() {
+        List<Integer> times = Arrays.asList(55, 99, 97, 93);
+        List<Integer> distances = Arrays.asList(401, 1485, 2274, 1405);
+        Approvals.verify(Day6.multipleOfNumberOfWaysToBeatRecord(times, distances));
+    }
+
+    @Test
     void multipleOfNumberOfWaysToBeatRecord() {
         List<Integer> times = Arrays.asList(7, 15, 30);
         List<Integer> distances = Arrays.asList(9, 40, 200);
