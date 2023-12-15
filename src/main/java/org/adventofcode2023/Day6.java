2cf87b6acae84f4e029144bf20866456178f6423
Susan Fung <fung_susan@hotmail.com>
2023-12-15 14:08:38 -0700

    . F Answer to Day 6 Part 2

--- src/main/java/org/adventofcode2023/Day6.java
+++ src/main/java/org/adventofcode2023/Day6.java
@@ -3,17 +3,17 @@ package org.adventofcode2023;
 import java.util.List;
 
 public class Day6 {
-    public static Integer multipleOfNumberOfWaysToBeatRecord(List<Integer> raceTimes, List<Long> winningDistanceRecords) {
-        int answer = 1;
+    public static Long multipleOfNumberOfWaysToBeatRecord(List<Long> raceTimes, List<Long> winningDistanceRecords) {
+        long answer = 1;
 
         for (int i = 0; i < raceTimes.size(); i++) {
-            int raceTime = raceTimes.get(i);
+            long raceTime = raceTimes.get(i);
             long distanceToBeat = winningDistanceRecords.get(i);
 
             int numberOfWaysToWin = 0;
 
             for (int holdTime = 0; holdTime <= raceTime; holdTime++) {
-                int distanceTraveled = holdTime * (raceTime - holdTime);
+                long distanceTraveled = holdTime * (raceTime - holdTime);
 
                 if (distanceToBeat < distanceTraveled) {
                     numberOfWaysToWin++;
--- src/test/java/org/adventofcode2023/Day6Test.java
+++ src/test/java/org/adventofcode2023/Day6Test.java
@@ -9,28 +9,28 @@ import java.util.List;
 class Day6Test {
     @Test
     void answerDay6Part1() {
-        List<Integer> times = Arrays.asList(55, 99, 97, 93);
+        List<Long> times = Arrays.asList(55L, 99L, 97L, 93L);
         List<Long> distances = Arrays.asList(401L, 1485L, 2274L, 1405L);
         Approvals.verify(Day6.multipleOfNumberOfWaysToBeatRecord(times, distances));
     }
 
     @Test
     void answerDay6Part2() {
-        List<Integer> times = Arrays.asList(55999793);
+        List<Long> times = Arrays.asList(55999793L);
         List<Long> distances = Arrays.asList(401148522741405L);
         Approvals.verify(Day6.multipleOfNumberOfWaysToBeatRecord(times, distances));
     }
 
     @Test
     void multipleOfNumberOfWaysToBeatRecordPart1() {
-        List<Integer> times = Arrays.asList(7, 15, 30);
+        List<Long> times = Arrays.asList(7L, 15L, 30L);
         List<Long> distances = Arrays.asList(9L, 40L, 200L);
         Approvals.verify(Day6.multipleOfNumberOfWaysToBeatRecord(times, distances));
     }
 
     @Test
     void multipleOfNumberOfWaysToBeatRecordPart2() {
-        List<Integer> times = Arrays.asList(71530);
+        List<Long> times = Arrays.asList(71530L);
         List<Long> distances = Arrays.asList(940200L);
         Approvals.verify(Day6.multipleOfNumberOfWaysToBeatRecord(times, distances));
     }
