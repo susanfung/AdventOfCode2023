b08ad46d1921f4bdf7591da5f9e44db5cba87f31
Susan Fung <fung_susan@hotmail.com>
2023-12-15 14:20:54 -0700

    ! F Created countAndSortCharacters method

--- src/main/java/org/adventofcode2023/Day7.java
+++ src/main/java/org/adventofcode2023/Day7.java
@@ -1,4 +1,23 @@
 package org.adventofcode2023;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Day7 {
+    public static List<String> countAndSortCharacters(String string) {
+        Map<Character, Integer> charCountMap = new HashMap<>();
+
+        for (char character : string.toCharArray()) {
+            charCountMap.put(character, charCountMap.getOrDefault(character, 0) + 1);
+        }
+
+        List<String> sortedCounts = charCountMap.values()
+                                            .stream()
+                                            .sorted((count1, count2) -> Integer.compare(count2, count1))
+                                            .map(String::valueOf)
+                                            .collect(java.util.stream.Collectors.toList());
+        return sortedCounts;
+    }
 }
--- src/test/java/org/adventofcode2023/Day7Test.java
+++ src/test/java/org/adventofcode2023/Day7Test.java
@@ -1,5 +1,15 @@
 package org.adventofcode2023;
 
-class Day7Test {
+import org.approvaltests.Approvals;
+import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+class Day7Test {
+    @Test
+    void countAndSortCharacters() {
+        String string = "32T3K";
+        Approvals.verifyAll("Character Counts", Day7.countAndSortCharacters(string));
+    }
 }
